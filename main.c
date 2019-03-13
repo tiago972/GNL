64bef30edd04c0769079f2d20e9022a7b5d7e702
tiago972 <edouard.baudouin@hotmail.fr>
2019-03-13 06:55:16 +0100

    test avec deux fichiers

--- main.c
+++ main.c
@@ -7,9 +7,14 @@ int		main()
 
 	int fd = open("test", O_RDWR);
 	while ((res = get_next_line(fd, &line)) > 0)
-		printf("%s\nres %d\n", line, res);
-	printf("%s\nres %d\n", line, res);
-	free(line);
+		//printf("%s\n", line);
 	close(fd);
+	char *line2;
+	int fd2 = open("test2", O_RDWR);
+	while ((res = get_next_line(fd2, &line2)) > 0)
+		//printf("%s\n", line2);
+	//free(line);
+	//free(line2);
+	close(fd2);
 	return (0);
 }
