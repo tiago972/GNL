192f2f2fb0e1ad1a6d5973aedce3903dabf7735c
tiago972 <edouard.baudouin@hotmail.fr>
2019-03-15 20:23:03 +0100

    sans le fichier auteur de la lib

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -4,8 +4,6 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
-# include <fcntl.h>
-
 void				ft_putchar(char	c);
 void				ft_putstr(const char *str);
 char				*ft_strdup(char *str);
@@ -72,6 +70,5 @@ void				ft_lstadd(t_list **alst, t_list *new_elem);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 void				*ft_realloc(void *ptr, size_t new_size);
-int					ft_strlendl(char *s);
 
 #endif
--- libft/srcs/ft_lstadd.c
+++ libft/srcs/ft_lstadd.c
@@ -2,6 +2,8 @@
 
 void	ft_lstadd(t_list **alst, t_list *new_elem)
 {
+	if (!new_elem)
+		return ;
 	new_elem->next = *alst;
 	*alst = new_elem;
 }
